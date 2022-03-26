e47f9d276b06e531196dda63961a3a5b6ef44542
moonk <mkjoehsb@gmail.com>
2022-03-26 17:40:42 +0900

    find device

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,45 +3,69 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
 let myStream;
 let muted = false;
-let cameraOff=false;
+let cameraOff = false;
 
-async function getMedia(){
+async function getCameras(){
   try{
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter(device=> device.kind ==="videoinput")
+    cameras.forEach(camera => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    })
+    console.log(cameras);
+    } catch(e){
+    console.log(e);
+  }
+}
+async function getMedia() {
+  try {
     myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true, 
-      video:true,
+      audio: true,
+      video: true,
     });
-    console.log(myStream);
+
     myFace.srcObject = myStream;
-  } catch(e) {
+    await getCameras();
+  } catch (e) {
     console.log(e);
   }
 }
 
 getMedia();
 
-function handleMuteClick(){
-  if(!muted){
-    muteBtn.innerText="Unmuted";
+function handleMuteClick() {
+  myStream
+  .getAudioTracks()
+  .forEach((track) => (track.enabled = !track.enabled));
+
+  if (!muted) {
+    muteBtn.innerText = "Unmuted";
     muted = true;
   } else {
-    muteBtn.innerText="Mute";
+    muteBtn.innerText = "Mute";
     muted = false;
   }
-}
-function handleCameraClick(){
+} 
 
-  if(cameraOff){
+function handleCameraClick() {
+  myStream
+  .getVideoTracks()
+  .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
-    cameraOff= false;
-  }
-  else {
+    cameraOff = false;
+  } else {
     cameraBtn.innerText = "Turn Camera On";
     cameraOff = true;
   }
 }
 
-muteBtn.addEventListener("click",handleMuteClick);
-cameraBtn.addEventListener("click",handleCameraClick);
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
