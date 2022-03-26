d001dec3334713385da45d8af6e2d829b4db82ca
moonk <mkjoehsb@gmail.com>
2022-03-26 16:48:51 +0900

    video call: call control

--- src/server.js
+++ src/server.js
@@ -16,30 +16,4 @@ const handleListen = (e) =>
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
-wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(`socket event: ${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    done();
-    console.log(socket.rooms);
-    socket.to(roomName).emit("welcome", socket.nickname); // broadcast
-  });
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
-  });
-
-  socket.on("new_message", (msg,room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-
-  socket.on("nickname", (nickname) =>{
-    socket["nickname"] = nickname;
-  });
-});
-
 httpServer.listen(3000, handleListen);
